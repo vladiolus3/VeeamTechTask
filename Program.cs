9f6db064c25a36a04b2f8f092bc8e15e554d3753
vladiolus3 <75817876+vladiolus3@users.noreply.github.com>
2025-05-15 00:22:11 +0200

    add synchronizer without loggings and file operations

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using VeeamTechTask;
+
+var cts = new CancellationTokenSource();
+var ct = cts.Token;
+
+var synchronizerApp = new SynchronizerApp();
+
+await synchronizerApp.RunAsync(args, ct);
+
+AppDomain.CurrentDomain.ProcessExit += (sender, e) => cts.Cancel();
