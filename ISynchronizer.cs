b5d4217331c829fc084b5fea2c4282149b5f7d4d
vladiolus3 <75817876+vladiolus3@users.noreply.github.com>
2025-05-15 17:18:06 +0200

    add CheckDirectories method

--- ISynchronizer.cs
+++ ISynchronizer.cs
@@ -10,7 +10,7 @@ namespace VeeamTechTask
 
     internal class Synchronizer(ILogger logger) : ISynchronizer
     {
-        public void Synchronize(string source, string destination) 
+        public void Synchronize(string source, string destination)
         {
             if (Path.IsPathRooted(source))
                 source = Path.GetFullPath(source);
@@ -34,6 +34,55 @@ namespace VeeamTechTask
 
             }
 
+            CheckDirectories(sourceDirInfo, destinationDirInfo);
+            CheckFiles(sourceDirInfo, destinationDirInfo);
+
+        }
+
+        #region directories
+
+        private void CheckDirectories(DirectoryInfo sourceDir, DirectoryInfo destinationDir)
+        {
+            var allSourceDirectoriesDict = GetDiretoriesWithRelativePaths(sourceDir);
+            var allDestinationDirectoriesDict = GetDiretoriesWithRelativePaths(destinationDir);
+
+            var directoriesToRemove = allDestinationDirectoriesDict.Keys.Where(k => !allSourceDirectoriesDict.ContainsKey(k));
+
+            foreach (var dirPath in directoriesToRemove)
+            {
+                var replicaDirName = allDestinationDirectoriesDict[dirPath];
+                var dirInfo = new DirectoryInfo(replicaDirName); // directory info doen`t update Exists status
+                if (dirInfo.Exists)
+                {
+                    dirInfo.Delete(true);
+                    logger.Debug("Directory {dir} is deleted", dirInfo.FullName);
+
+                }
+
+            }
+        }
+
+        private static Dictionary<string, string> GetDiretoriesWithRelativePaths(DirectoryInfo directory)
+        {
+            var directories = directory.GetDirectories("*", SearchOption.AllDirectories);
+            var directoriesDict = new Dictionary<string, string>();
+
+            foreach (var dir in directories)
+            {
+                var key = Path.GetRelativePath(directory.FullName, dir.FullName);
+                directoriesDict[key] = dir.FullName;
+
+            }
+
+            return directoriesDict;
+
+        }
+
+        #endregion
+
+        #region files
+        private void CheckFiles(DirectoryInfo sourceDirInfo, DirectoryInfo destinationDirInfo)
+        {
             var allSourceFilesDict = GetFilesWithRelativePaths(sourceDirInfo);
             var allDestinationFilesDict = GetFilesWithRelativePaths(destinationDirInfo);
 
@@ -45,20 +94,20 @@ namespace VeeamTechTask
                 {
                     logger.Information("File {file} is not readable", sourceFile.Value.FullName);
                     continue;
-                
+
                 }
 
                 var destinationFileExists = allDestinationFilesDict.TryGetValue(sourceFile.Key, out var replicaFile);
 
-                var destinationFileUpdated = !destinationFileExists 
-                    || replicaFile!.LastWriteTime != sourceFile.Value.LastWriteTime 
+                var destinationFileUpdated = !destinationFileExists
+                    || replicaFile!.LastWriteTime != sourceFile.Value.LastWriteTime
                     || !replicaFile.EqualsTo(sourceFile.Value);
 
-                if (destinationFileUpdated) 
+                if (destinationFileUpdated)
                 {
                     if (!destinationFileExists)
                         logger.Debug("File {file} is created", destinationFullPath);
-                    else 
+                    else
                         logger.Debug("File {file} is updated", destinationFullPath);
 
                     sourceFile.Value.Upsert(destinationFullPath);
@@ -69,7 +118,7 @@ namespace VeeamTechTask
 
             var filesToRemove = allDestinationFilesDict.Keys.Where(k => !allSourceFilesDict.ContainsKey(k));
 
-            foreach (var filePath in filesToRemove) 
+            foreach (var filePath in filesToRemove)
             {
                 var replicaFile = allDestinationFilesDict[filePath];
 
@@ -79,13 +128,12 @@ namespace VeeamTechTask
                     logger.Debug("File {file} is deleted", replicaFile.FullName);
 
             }
-
         }
 
         private static Dictionary<string, FileInfo> GetFilesWithRelativePaths(DirectoryInfo directory)
         {
             var files = directory.GetFiles("*", SearchOption.AllDirectories);
-            var filesDict = new Dictionary<string, FileInfo>(); 
+            var filesDict = new Dictionary<string, FileInfo>();
 
             foreach (var file in files)
             {
@@ -95,8 +143,10 @@ namespace VeeamTechTask
             }
 
             return filesDict;
-            
+
         }
+
+        #endregion
 
     }
 
